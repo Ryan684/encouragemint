27dc7cf2aa4bd540de8320a75bc2087159109fa2
RyanK1993 <rk1g13@soton.ac.uk>
2019-11-28 22:31:38 +0000

    WIP - Trefle preperation/beginning of code design.

---
+++ encouragemint/lib/trefle/tests/test_trefle.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestTrefle(TestCase):
+
+    def test_lookup_plant_one_match(self):
+        self.assertTrue(True)
+
+    def test_lookup_plant_many_matches(self):
+        self.assertTrue(True)
+
+    def test_lookup_plant_no_matches(self):
+        self.assertTrue(True)
+
+    def test_lookup_plant_trefle_down(self):
+        self.assertTrue(True)
