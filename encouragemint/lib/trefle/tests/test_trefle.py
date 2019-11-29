50f629c518b44b5f695c42cf02440383e0226671
RyanK1993 <rk1g13@soton.ac.uk>
2019-11-29 22:39:14 +0000

    WIP - Trefle Plant lookup code.

--- encouragemint/lib/trefle/tests/test_trefle.py
+++ encouragemint/lib/trefle/tests/test_trefle.py
@@ -8,27 +8,40 @@ class TestTrefle(TestCase):
         self.trefle = TrefleAPI()
 
     def test_lookup_plant_one_match(self):
+        field = "common_name"
         plant_name = "common woolly sunflower"
+        response = self.trefle.lookup_plant(field, plant_name)
+
+        self._validate_plant(response)
+
+    def test_lookup_plant_by_scientific_name(self):
+        field = "scientific_name"
+        plant_name = "Eriophyllum lanatum"
+        response = self.trefle.lookup_plant(field, plant_name)
+
+        self._validate_plant(response)
+
+    def _validate_plant(self, response):
         expected_plant = {
             "trefle_id": 134845,
-            "scientific_name": "Eriophyllum lanatum",
+            "common_name": "common woolly sunflower",
             "duration": "Annual, Perennial",
             "bloom_period": None,
             "growth_period": None,
             "growth_rate": None,
             "shade_tolerance": None,
             "moisture_use": None,
-            "family_common_name": "Aster family"
+            "family_common_name": "Aster family",
+            "scientific_name": "Eriophyllum lanatum"
         }
-
-        response = self.trefle.lookup_plant(plant_name)
-
+        
         self.assertEqual(expected_plant.get("trefle_id"), response.get("trefle_id"))
         self.assertEqual(expected_plant.get("scientific_name"), response.get("scientific_name"))
+        self.assertEqual(expected_plant.get("common_name"), response.get("common_name"))
         self.assertEqual(expected_plant.get("duration"), response.get("duration"))
         self.assertEqual(expected_plant.get("bloom_period"), response.get("bloom_period"))
         self.assertEqual(expected_plant.get("growth_period"), response.get("growth_period"))
         self.assertEqual(expected_plant.get("growth_rate"), response.get("growth_rate"))
         self.assertEqual(expected_plant.get("shade_tolerance"), response.get("shade_tolerance"))
         self.assertEqual(expected_plant.get("moisture_use"), response.get("moisture_use"))
-        self.assertEqual(expected_plant.get("family_name"), response.get("family_name"))
+        self.assertEqual(expected_plant.get("family_common_name"), response.get("family_common_name"))
--- encouragemint/lib/trefle/trefle.py
+++ encouragemint/lib/trefle/trefle.py
@@ -6,31 +6,43 @@ class TrefleAPI:
     HEADERS = {"content-type": "application/json"}
     TOKEN = "aUF2TXNmazZhbENpTCtJWkhqTUIvUT09"
 
-    def lookup_plant(self, plant_name):
-        results = self._send_trefle_request(
-            self._compile_parameters("common_name", plant_name),
+    def lookup_plant(self, name_key, plant_name):
+        matched_plants = self._lookup_plants_by_name(name_key, plant_name)
+
+        if len(matched_plants) == 1:
+            plant = self._lookup_plant_by_id(matched_plants)
+            return self._extract_plant_data(plant)
+
+        return matched_plants
+
+    def _lookup_plants_by_name(self, name_key, plant_name):
+        return self._send_trefle_request(
+            self._compile_parameters(name_key, plant_name),
             self._compile_url()
         ).json()
-        if len(results) == 1:
-            plant = self._send_trefle_request(
-                self._compile_parameters(),
-                self._compile_url(results[0].get("id"))
-            ).json()
-            return self._extract_plant_data(plant)
-        return results
+
+    def _lookup_plant_by_id(self, results):
+        return self._send_trefle_request(
+            self._compile_parameters(),
+            self._compile_url(results[0].get("id"))
+        ).json()
 
     def _compile_parameters(self, key=None, value=None):
         params = {
             "token": self.TOKEN
         }
+
         if key and value:
             params[key] = value
+
         return params
 
     def _compile_url(self, plant_id=None):
         url = self.PLANTS_ENDPOINT
+
         if plant_id:
             url = url + str(plant_id)
+
         return url
 
     def _send_trefle_request(self, parameters, url):
@@ -43,15 +55,15 @@ class TrefleAPI:
         return response
 
     def _extract_plant_data(self, plant):
-        # TEMP
         return {
-            "trefle_id": 134845,
-            "scientific_name": "Eriophyllum lanatum",
-            "duration": "Annual, Perennial",
-            "bloom_period": None,
-            "growth_period": None,
-            "growth_rate": None,
-            "shade_tolerance": None,
-            "moisture_use": None,
-            "family_common_name": "Aster family"
+            "trefle_id": plant.get("id"),
+            "common_name": plant.get("common_name"),
+            "duration": plant.get("duration"),
+            "bloom_period": plant.get("main_species").get("seed").get("bloom_period"),
+            "growth_period": plant.get("main_species").get("specifications").get("growth_period"),
+            "growth_rate": plant.get("main_species").get("specifications").get("growth_rate"),
+            "shade_tolerance": plant.get("main_species").get("growth").get("shade_tolerance"),
+            "moisture_use": plant.get("main_species").get("growth").get("moisture_use"),
+            "scientific_name": plant.get("scientific_name"),
+            "family_common_name": plant.get("family_common_name")
         }
