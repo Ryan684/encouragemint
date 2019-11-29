7df0dcac5236dab8ca3054ad904fa71fb6b7c31c
RyanK1993 <rk1g13@soton.ac.uk>
2019-11-29 21:28:57 +0000

    WIP - Trefle Plant lookup code.

--- encouragemint/lib/trefle/tests/test_trefle.py
+++ encouragemint/lib/trefle/tests/test_trefle.py
@@ -8,13 +8,27 @@ class TestTrefle(TestCase):
         self.trefle = TrefleAPI()
 
     def test_lookup_plant_one_match(self):
-        self.assertTrue(True)
+        plant_name = "common woolly sunflower"
+        expected_plant = {
+            "trefle_id": 134845,
+            "scientific_name": "Eriophyllum lanatum",
+            "duration": "Annual, Perennial",
+            "bloom_period": None,
+            "growth_period": None,
+            "growth_rate": None,
+            "shade_tolerance": None,
+            "moisture_use": None,
+            "family_common_name": "Aster family"
+        }
 
-    def test_lookup_plant_many_matches(self):
-        self.assertTrue(True)
+        response = self.trefle.lookup_plant(plant_name)
 
-    def test_lookup_plant_no_matches(self):
-        self.assertTrue(True)
-
-    def test_lookup_plant_trefle_down(self):
-        self.assertTrue(True)
+        self.assertEqual(expected_plant.get("trefle_id"), response.get("trefle_id"))
+        self.assertEqual(expected_plant.get("scientific_name"), response.get("scientific_name"))
+        self.assertEqual(expected_plant.get("duration"), response.get("duration"))
+        self.assertEqual(expected_plant.get("bloom_period"), response.get("bloom_period"))
+        self.assertEqual(expected_plant.get("growth_period"), response.get("growth_period"))
+        self.assertEqual(expected_plant.get("growth_rate"), response.get("growth_rate"))
+        self.assertEqual(expected_plant.get("shade_tolerance"), response.get("shade_tolerance"))
+        self.assertEqual(expected_plant.get("moisture_use"), response.get("moisture_use"))
+        self.assertEqual(expected_plant.get("family_name"), response.get("family_name"))
--- encouragemint/lib/trefle/trefle.py
+++ encouragemint/lib/trefle/trefle.py
@@ -1,3 +1,57 @@
-class TrefleAPI():
-    def wip(self):
-        return "foo!"
+from requests import get
+
+
+class TrefleAPI:
+    PLANTS_ENDPOINT = "http://trefle.io/api/plants/"
+    HEADERS = {"content-type": "application/json"}
+    TOKEN = "aUF2TXNmazZhbENpTCtJWkhqTUIvUT09"
+
+    def lookup_plant(self, plant_name):
+        results = self._send_trefle_request(
+            self._compile_parameters("common_name", plant_name),
+            self._compile_url()
+        ).json()
+        if len(results) == 1:
+            plant = self._send_trefle_request(
+                self._compile_parameters(),
+                self._compile_url(results[0].get("id"))
+            ).json()
+            return self._extract_plant_data(plant)
+        return results
+
+    def _compile_parameters(self, key=None, value=None):
+        params = {
+            "token": self.TOKEN
+        }
+        if key and value:
+            params[key] = value
+        return params
+
+    def _compile_url(self, plant_id=None):
+        url = self.PLANTS_ENDPOINT
+        if plant_id:
+            url = url + str(plant_id)
+        return url
+
+    def _send_trefle_request(self, parameters, url):
+        response = get(
+            url=url,
+            headers=self.HEADERS,
+            params=parameters,
+            verify=False
+        )
+        return response
+
+    def _extract_plant_data(self, plant):
+        # TEMP
+        return {
+            "trefle_id": 134845,
+            "scientific_name": "Eriophyllum lanatum",
+            "duration": "Annual, Perennial",
+            "bloom_period": None,
+            "growth_period": None,
+            "growth_rate": None,
+            "shade_tolerance": None,
+            "moisture_use": None,
+            "family_common_name": "Aster family"
+        }
