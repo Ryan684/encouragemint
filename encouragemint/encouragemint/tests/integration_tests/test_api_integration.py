c2e2e15999c96d467215330ee93662210219161c
RyanK1993 <rk1g13@soton.ac.uk>
2020-01-28 13:22:08 +0000

    WIP Setup for integration tests.

---
+++ encouragemint/encouragemint/tests/integration_tests/test_api_integration.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+# TODO: Write integration tests
+
+
+class TestProfile(TestCase):
+    def test_create_profile(self):
+        pass
+
+    def test_retrieve_profile(self):
+        pass
+
+    def test_list_profiles(self):
+        pass
+
+    def test_delete_profile(self):
+        pass
+
+    def test_patch_profile(self):
+        pass
+
+    def test_update_profile(self):
+        pass
+
+
+class TestGarden(TestCase):
+    def test_create_garden(self):
+        pass
+
+    def test_delete_garden(self):
+        pass
+
+    def test_patch_garden(self):
+        pass
+
+    def test_update_garden(self):
+        pass
+
+
+class TestPlant(TestCase):
+    def test_create_plant(self):
+        pass
+
+    def test_delete_plant(self):
+        pass
+
+    def test_update_garden(self):
+        pass
