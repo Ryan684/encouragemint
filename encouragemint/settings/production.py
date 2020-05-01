e16680eba018abee022522fcee7f793c95104f8d
RyanK1993 <rk1g13@soton.ac.uk>
2020-05-01 16:41:53 +0100

    WIP Inclusion of MySQL DB Container.

--- encouragemint/settings/production.py
+++ encouragemint/settings/production.py
@@ -1,3 +1,19 @@
+import pymysql
+
 from encouragemint.settings.base import *
 
 DEBUG = False
+
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ["MYSQL_DATABASE"],
+        "USER": os.environ["MYSQL_USER"],
+        "PASSWORD": os.environ["MYSQL_PASSWORD"],
+        "HOST": "db",
+        "PORT": "3306"
+    }
+}
