5342ebdd8dff120c0f12a505d1fc5df7df236da3
RyanK1993 <rk1g13@soton.ac.uk>
2020-05-25 21:41:23 +0100

    Addition of django cors config.

--- encouragemint/settings/base.py
+++ encouragemint/settings/base.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'encouragemint'
+    'encouragemint',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
 
 ROOT_URLCONF = 'encouragemint.urls'
 
