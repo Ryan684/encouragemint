93cf04d2f278a7e65ffa29caa26d8fb18d59ee65
RyanK1993 <rk1g13@soton.ac.uk>
2020-06-13 13:44:12 +0100

    Some React practice.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { ProfileRegistrationForm } from './profileForm';
+import { ProfileRegistrationForm } from './profileRegistrationForm';
 
 function App() {
   return (
