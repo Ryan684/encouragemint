8886a36320aec92da78c4b596a29f2ef64b7e981
RyanK1993 <rk1g13@soton.ac.uk>
2020-06-17 18:34:23 +0100

    Some React practice.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import { ProfileRegistrationForm } from './profileRegistrationForm';
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <ProfileRegistrationForm />
-      </header>
+        <Container>
+          <Row>
+            <Col />
+            <Col xs={8}>
+                  <header className="App-header">
+                    <h1>Create a Profile</h1>
+                    <ProfileRegistrationForm />
+                  </header>
+            </Col>
+            <Col />
+          </Row>
+        </Container>
     </div>
   );
 }
