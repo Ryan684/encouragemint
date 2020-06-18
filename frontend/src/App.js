629b379d40c55fe6160243266e8a1d6f64f324f3
RyanK1993 <rk1g13@soton.ac.uk>
2020-06-18 15:12:42 +0100

    Rolling back after doing some react practice, slimming down the create-react-app stuff that i won't need.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import { ProfileRegistrationForm } from './profileRegistrationForm';
 
-export function App() {
+function App() {
   return (
     <div className="App">
-        <Container>
-          <Row>
-            <Col />
-            <Col xs={8}>
-                  <header className="App-header">
-                    <h1>Create a Profile</h1>
-                    <ProfileRegistrationForm />
-                  </header>
-            </Col>
-            <Col />
-          </Row>
-        </Container>
     </div>
   );
 }
+
+export default App;
