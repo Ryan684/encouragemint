1bbf92e9e6ff12804f0718df478495dad6602ec1
RyanK1993 <rk1g13@soton.ac.uk>
2020-07-01 20:14:05 +0100

    WIP Signup form validation and general tweaks from learning.

--- frontend/src/Copyright.js
+++ frontend/src/Copyright.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 
 export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Encouragemint
-      </Link>{' '}
+      {'Copyright © Encouragemint '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
