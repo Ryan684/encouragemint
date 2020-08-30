11f22c0de33b5b0a05f58aab6e7b52bdb13ccb85
RyanK1993 <rk1g13@soton.ac.uk>
2020-08-30 18:00:34 +0100

    Added module to poc to lookup coordinates for a given address

---
+++ recommend/exceptions.py
@@ -0,0 +1,2 @@
+class GeocoderNoResultsError(Exception):
+    pass
---
+++ recommend/tests/test_locator.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch, Mock
+
+from django.test import TestCase
+
+from backend.tests.helpers import SAMPLE_GARDEN_GEOCODE_LOCATION
+from recommend.exceptions import GeocoderNoResultsError
+from recommend.locator import get_coordinates
+
+
+class TestGetCoordinates(TestCase):
+    def setUp(self):
+        patcher = patch("geopy.geocoders.googlev3.GoogleV3.geocode")
+        self.mock_geocoder = patcher.start()
+        self.addCleanup(patcher.stop)
+        self.dummy_location = "Romsey, Hampshire"
+
+    def test_no_results(self):
+        self.mock_geocoder.return_value = None
+
+        self.assertRaises(GeocoderNoResultsError, get_coordinates, self.dummy_location)
+
+    def test_location_found(self):
+        mock = Mock(**SAMPLE_GARDEN_GEOCODE_LOCATION)
+        self.mock_geocoder.return_value = mock
+
+        latitude, longitude, address = get_coordinates(self.dummy_location)
+
+        self.assertEqual(SAMPLE_GARDEN_GEOCODE_LOCATION["latitude"], latitude)
+        self.assertEqual(SAMPLE_GARDEN_GEOCODE_LOCATION["longitude"], longitude)
+        self.assertEqual(SAMPLE_GARDEN_GEOCODE_LOCATION["address"], address)
