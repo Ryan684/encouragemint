ee21679d36026d42fec82c86faa5862547122bf2
ryan684 <ryankemp1993@hotmail.co.uk>
2021-05-05 13:43:43 +0100

    Added frontend kubernetes files

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -11,7 +11,8 @@ METEOSTAT_API_KEY = os.getenv("METEOSTAT_API_KEY")
 CURRENT_HOST = os.getenv("NODE_IP", "127.0.0.1")
 ALLOWED_HOSTS = [
     CURRENT_HOST,
-    ".elb.amazonaws.com"
+    ".elb.amazonaws.com",
+    "127.0.0.1"
 ]
 
 
