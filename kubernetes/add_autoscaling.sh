1c6f07c69740bcb86811309c26aa86b15e0705d3
ryan684 <ryankemp1993@hotmail.co.uk>
2021-05-26 21:08:07 +0100

    Adding horizontal pod scaling for backend based on cpu.

--- kubernetes/add_autoscaling.sh
+++ kubernetes/add_autoscaling.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-kubectl
- apply -f horizontal_autoscaler.yaml
+kubectl apply -f horizontal_autoscaler.yaml
